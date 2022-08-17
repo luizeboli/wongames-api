32cc8e34653f7eb0729e99b194660c3c77e546c4
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2022-08-17 17:37:00 -0300

    refactor(master): remove wishlist rest routes and create core controller

--- src/api/wishlist/controllers/wishlist.js
+++ src/api/wishlist/controllers/wishlist.js
@@ -1,24 +1,3 @@
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::wishlist.wishlist', {
-  async create(ctx) {
-    const userToken = await strapi.plugins['users-permissions'].services.jwt.getToken(ctx);
-
-    const body = {
-      ...ctx.request.body,
-      user: userToken.id,
-    };
-
-    const entity = await strapi.services.wishlist.create(body);
-    return await this.sanitizeOutput(entity, ctx);
-  },
-
-  async update(ctx) {
-    try {
-      const entity = await strapi.services.wishlist.update({ id: ctx.params.id }, ctx.request.body);
-      return await this.sanitizeOutput(entity, ctx);
-    } catch (error) {
-      throw strapi.errors.unauthorized(error);
-    }
-  },
-});
+module.exports = createCoreController('api::wishlist.wishlist');
