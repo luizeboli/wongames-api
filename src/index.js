33af579c7f8cf881d7d52f01b3329d8d193a8161
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2022-09-08 13:55:40 -0300

    refactor(master): milestone to save tries

--- src/index.js
+++ src/index.js
@@ -1,63 +1,41 @@
 'use strict';
 
-const { ForbiddenError } = require('@strapi/utils').errors;
-
-const checkIfIsTryingToAccessOtherUser = (user, filterUser) => {
-  if (!filterUser) return false;
-  const errors = [];
-
-  if (filterUser.id) {
-    errors.push(Object.values(filterUser.id).some((id) => Number(id) !== user.id));
-  }
-
-  if (filterUser.username) {
-    errors.push(Object.values(filterUser.username).some((username) => username !== user.username));
-  }
-
-  if (filterUser.email) {
-    errors.push(Object.values(filterUser.email).some((email) => email !== user.email));
-  }
-
-  return !!errors.filter(Boolean).length;
-};
-
 module.exports = {
   register({ strapi }) {
     const extensionService = strapi.service('plugin::graphql.extension');
+    const { toEntityResponseCollection, toEntityResponse } = strapi.plugin('graphql').service('format').returnTypes;
 
-    extensionService.use(({ strapi }) => ({
-      resolversConfig: {
-        'Query.wishlists': {
-          middlewares: [
-            async (next, parent, args, context, info) => {
-              // This condition will never be true because this route is only accessible to authenticated users,
-              // through users-permissions plugin
-              // It's just an example of how to use custom errors
-              if (!context.state.isAuthenticated) {
-                throw new ForbiddenError('You are not allowed to do this');
-              }
+    extensionService.use(({ nexus }) => ({
+      types: [
+        nexus.extendType({
+          type: 'UsersPermissionsMe',
+          definition(t) {
+            t.field('wishlists', {
+              type: 'WishlistEntityResponseCollection',
+              resolve: async (root, args) => {
+                const userData = await strapi.entityService.findMany('api::wishlist.wishlist', {
+                  filters: { user: root.id },
+                });
 
-              const isTryingToFetchOtherUserData = checkIfIsTryingToAccessOtherUser(context.state.user, args.filters?.user);
-
-              if (isTryingToFetchOtherUserData) {
-                return {
-                  value: {
-                    data: null,
-                  },
-                };
-              }
-
-              const res = await next(
-                parent,
-                { ...args, filters: { ...args.filters, user: { id: { eq: context.state.user.id } } } },
-                context,
-                info,
-              );
-              return res;
-            },
-          ],
-        },
-      },
+                return toEntityResponseCollection(userData ?? [], {
+                  args,
+                  resourceUID: 'api::wishlist.wishlist',
+                });
+              },
+            });
+          },
+        }),
+        nexus.mutationField('updateWishlist', {
+          type: nexus.nonNull('WishlistEntityResponse'),
+          args: {
+            data: nexus.nonNull('WishlistInput'),
+          },
+          resolve(root, args, ctx) {
+            console.log({ root, args, ctx }, 'oi');
+            return null;
+          },
+        }),
+      ],
       typeDefs: `
         input WishlistCreateInput {
           games: [ID]
@@ -71,7 +49,6 @@ module.exports = {
         Mutation: {
           createWishlist: {
             async resolve(parent, args, context) {
-              const { toEntityResponse } = strapi.service('plugin::graphql.format').returnTypes;
               const createdWishlist = await strapi.entityService.create('api::wishlist.wishlist', {
                 data: {
                   user: context.state.user.id,
@@ -82,6 +59,14 @@ module.exports = {
               return toEntityResponse(createdWishlist, { args, resourceUID: 'api::wishlist.wishlist' });
             },
           },
+        },
+      },
+      resolversConfig: {
+        'Mutation.updateWishlist': {
+          policies: ['global::is-owner'],
+        },
+        'Mutation.createWishlist': {
+          policies: ['global::is-owner'],
         },
       },
     }));
