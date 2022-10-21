c0010b6db2796e42363995d61f34bdb244f2b1b8
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2022-10-21 11:03:10 -0300

    Trying to fix cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -44,7 +44,7 @@ module.exports = () => [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['*.felicio.dev'],
+      origin: ['wongames.felicio.dev', 'wongames-api.felicio.dev'],
       methods: ['GET', 'POST', 'PUT', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
