964e1af5158f87eeba2543fa5bc6109b2bcbaab4
Luiz Felicio <13091635+luizeboli@users.noreply.github.com>
2022-10-21 10:49:13 -0300

    Use wildcard for cors config

--- config/middlewares.js
+++ config/middlewares.js
@@ -44,7 +44,7 @@ module.exports = () => [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['wongames.felicio.dev'],
+      origin: ['*.felicio.dev'],
       methods: ['GET', 'POST', 'PUT', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
